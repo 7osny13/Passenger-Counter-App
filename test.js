3fc8a0c1cbd9e47de8c095710c16ce1bb4d4466d
Your Name <you@example.com>
2022-10-21 10:04:59 +0200

    simple passenger counter app

---
+++ test.js
@@ -0,0 +1,108 @@
+
+// let message = "Hi , my name is ";
+
+// let  userName = "omar" ;
+
+// messageToUser = message + ", " + userName + "!" ; 
+
+// console.log(messageToUser) ;
+
+
+let welcomeEl = document.getElementById("welcome-el") ; 
+
+
+let name = "omar" ;
+
+let greeting = "welcome" ; 
+
+welcomeEl.innerText = greeting  + " "  + name ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("count-el").innerText = 5;
+
+
+// console.log(myAge); // reference error cannot access var before initialization 
+
+// let myAge  = 25;
+
+// console.log(myAge);
+
+
+
+// let myAge = 25  ; 
+
+// let humanDogRatio  =  7;
+
+// let myDogAge = myAge * humanDogRatio;
+
+// console.log(myDogAge)
+
+
+// let bounusPoints = 50;
+
+// bounusPoints += 50;
+
+// console.log(bounusPoints);
+
+// bounusPoints -= 25;
+
+// console.log(bounusPoints);
+
+// bounusPoints += 70;
+
+// console.log(bounusPoints)
+
+
+
+// function fortyTwo() {
+
+//     console.log(42);
+// }
+
+
+// fortyTwo();
+// fortyTwo();
+// fortyTwo();
+
+
+// let lab1 = 34;
+// let lab2 = 33;
+// let lab3 = 36;
+
+
+// function sum() {
+//     let sum = lab1 + lab2 + lab3;
+//     console.log(sum);
+// }
+
+// sum();
+
+
+// let labCompleted = 0 ;
+
+
+// function labIncrement(){
+//     labCompleted +=1;
+// }
+
+// labIncrement();
+// labIncrement();
+// labIncrement();
+
+
+// console.log(labCompleted);
